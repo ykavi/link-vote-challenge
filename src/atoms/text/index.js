02ba3b595193ed07e4529ddf6230b53467724b20
YUNUS EMRE KAVİ <yunus_emre_kavin@hotmail.com>
2021-07-31 02:12:56 +0300

    remove console log

--- src/atoms/text/index.js
+++ src/atoms/text/index.js
@@ -1,7 +1,6 @@
 import { StyledText } from "./styles.js";
 
 const Text = ({ size, weight, margin, text }) => {
-  console.log(size);
   return (
     <StyledText size={size} weight={weight} margin={margin}>
       {text}
